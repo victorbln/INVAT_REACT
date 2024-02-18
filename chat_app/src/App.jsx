2510a65991b04509d0eb66f34819e509742a3184
Victor Balan <victor.balan@assist.ro>
2024-02-18 18:27:41 +0200

    Styling for chat layout

--- chat_app/src/App.jsx
+++ chat_app/src/App.jsx
@@ -1,23 +1,22 @@
-import { ChatControls } from './components/chat-controls'
-import './App.css'
-import { ChatLayout } from './components/chat-layout'
-import { ChatDiscussionsList } from './components/chat-discussions-list'
-import { ChatMessageList } from './components/chat-message-list'
-import { ChatStartDiscussionModal } from './components/chat-start-discussion-modal'
+import { ChatControls } from "./components/chat-controls";
+import "./App.css";
+import { ChatLayout } from "./components/chat-layout";
+import { ChatDiscussionsList } from "./components/chat-discussions-list";
+import { ChatMessageList } from "./components/chat-message-list";
+import { ChatStartDiscussionModal } from "./components/chat-start-discussion-modal";
 
 function App() {
-
   return (
     <>
-   {true && <ChatStartDiscussionModal/>}
+      {false && <ChatStartDiscussionModal />}
 
-    <ChatLayout 
-      controls={<ChatControls/>}
-      aside={<ChatDiscussionsList/>}
-      main={<ChatMessageList/>}
+      <ChatLayout
+        controls={<ChatControls />}
+        aside={<ChatDiscussionsList />}
+        main={<ChatMessageList />}
       />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

c5cc32cf773c84653968fe059e903c868de70b3f
Victor Balan <victor.balan@assist.ro>
2024-02-18 18:41:29 +0200

    Add constants

---
+++ chat_app/src/constants/messages.js
@@ -0,0 +1,32 @@
+export const MESSAGES = [
+  {
+    id: 0,
+    discussionId: 0,
+    messages: [
+      {
+        id: 0,
+        value: "Hello, how are you?",
+        date: "Tue 23 Jan, 15:50",
+        author: "John Doe",
+      },
+      {
+        id: 1,
+        value: "I'm fine, thank you",
+        date: "Tue 23 Jan, 15:51",
+        author: "Emily Davis",
+      },
+      {
+        id: 2,
+        value: "What about you?",
+        date: "Tue 23 Jan, 15:52",
+        author: "Emily Davis",
+      },
+      {
+        id: 3,
+        value: "I'm fine too",
+        date: "Tue 23 Jan, 15:53",
+        author : "John Doe",
+      }
+    ],
+  },
+];
